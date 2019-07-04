8b009a3a2435c27d64818257f7740e53b521a908
XDMaxinrui <52492646+XDMaxinrui@users.noreply.github.com>
2019-07-04 22:40:23 +0800

    Update download.ino.cpp

--- 第二次作业/download.ino.cpp
+++ 第二次作业/download.ino.cpp
@@ -1,13 +1,13 @@
 #include <Keypad.h> 
 void setup()
 {
-  
-  pinMode (5, OUTPUT) ;
+  Serial.begin(9600);
+  pinMode (5, OUTPUT) ;//不接0-1引脚
   pinMode (6, OUTPUT) ;
   pinMode (7, OUTPUT) ;
   pinMode (8, OUTPUT) ;
   pinMode (9, OUTPUT) ; 
-  Serial.begin(9600);
+  
        
 }
 void loop()
@@ -17,18 +17,18 @@ void loop()
       digitalWrite (7, LOW) ;
       digitalWrite (8, LOW) ;
       digitalWrite (9, LOW) ;
-  int in_come=0;
+  int number=0;
   while(Serial.available()>0)
   {
   
-    in_come=Serial.read()-'0';
-    if((in_come&1)==1)
+    number=Serial.read()-'0';
+    if((number&1)==1)
     digitalWrite (5, HIGH) ;
-      if((in_come>>1&1)==1)
+      if((number>>1&1)==1)
     digitalWrite (6, HIGH) ;
-      if((in_come>>2&1)==1)
+      if((number>>2&1)==1)
     digitalWrite (7, HIGH) ;
-      if((in_come>>3&1)==1)
+      if((number>>3&1)==1)
     digitalWrite (8, HIGH) ;
     else
     digitalWrite (9, HIGH) ;
