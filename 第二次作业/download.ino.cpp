43090cb9154f2bf92d4aab482d6f7aad2ee9a1cd
XDMaxinrui <52492646+XDMaxinrui@users.noreply.github.com>
2019-07-04 21:37:22 +0800

    Update download.ino.cpp

--- 第二次作业/download.ino.cpp
+++ 第二次作业/download.ino.cpp
@@ -2,36 +2,36 @@
 void setup()
 {
   
-  pinMode (1, OUTPUT) ;
-  pinMode (2, OUTPUT) ;
-  pinMode (3, OUTPUT) ;
-  pinMode (4, OUTPUT) ;
-  pinMode (5, OUTPUT) ; 
+  pinMode (5, OUTPUT) ;
+  pinMode (6, OUTPUT) ;
+  pinMode (7, OUTPUT) ;
+  pinMode (8, OUTPUT) ;
+  pinMode (9, OUTPUT) ; 
   Serial.begin(9600);
        
 }
 void loop()
 {
       digitalWrite (5, LOW) ;
-      digitalWrite (1, LOW) ;
-      digitalWrite (2, LOW) ;
-      digitalWrite (3, LOW) ;
-      digitalWrite (4, LOW) ;
-  int income;
+      digitalWrite (6, LOW) ;
+      digitalWrite (7, LOW) ;
+      digitalWrite (8, LOW) ;
+      digitalWrite (9, LOW) ;
+  int income=0;
   while(Serial.available()>0)
   {
   
     income=Serial.read()-'0';
     if((income&1)==1)
-    digitalWrite (1, HIGH) ;
-      if((income>>1&1)==1)
-    digitalWrite (2, HIGH) ;
-      if((income>>2&1)==1)
-    digitalWrite (3, HIGH) ;
-      if((income>>3&1)==1)
-    digitalWrite (4, HIGH) ;
-    
     digitalWrite (5, HIGH) ;
+      if((income>>1&1)==1)
+    digitalWrite (6, HIGH) ;
+      if((income>>2&1)==1)
+    digitalWrite (7, HIGH) ;
+      if((income>>3&1)==1)
+    digitalWrite (8, HIGH) ;
+    else
+    digitalWrite (9, HIGH) ;
     delay(10);
 }
 }
